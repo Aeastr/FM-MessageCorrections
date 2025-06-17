320caf401a8f611ca9147d77c4406c893058ebd3
Aether <64797587+Aeastr@users.noreply.github.com>
2025-06-17 23:19:59 +0100

    Initial Commit

---
+++ FM-MessageCorrections/FM_MessageCorrectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FM_MessageCorrectionsApp.swift
+//  FM-MessageCorrections
+//
+//  Created by Aether on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FM_MessageCorrectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
